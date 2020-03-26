c5c03ba02b6b99e9df203f31579d4ff2ace0bfb6
Joshua <joecoolatbyu@hotmail.com>
2020-03-26 11:02:28 -0600

    implemented exception handling

--- src/main/java/com/test/hplus/controllers/HomeController.java
+++ src/main/java/com/test/hplus/controllers/HomeController.java
@@ -1,5 +1,6 @@
 package com.test.hplus.controllers;
 
+import com.test.hplus.beans.Login;
 import com.test.hplus.beans.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,37 +13,42 @@ import java.util.List;
 public class HomeController {
 
     @GetMapping("/home")
-    public String goHome(){
+    public String goHome() {
         System.out.println("in home controller");
         return "index";
     }
 
     @GetMapping("/goToSearch")
-    public String goToSearch(){
+    public String goToSearch() {
         System.out.println("going to search page");
         return "search";
     }
 
     @GetMapping("/goToLogin")
-    public String goToLogin(){
+    public String goToLogin() {
         System.out.println("going to login page");
         return "login";
     }
 
     @GetMapping("/goToRegistration")
-    public String goToRegistration(){
+    public String goToRegistration() {
         System.out.println("going to register page");
         return "register";
     }
 
     @ModelAttribute("newuser")
-    public User getDefaultUser(){
+    public User getDefaultUser() {
         return new User();
     }
 
     @ModelAttribute("genderItems")
-    public List<String> getGenderItems(){
+    public List<String> getGenderItems() {
         return Arrays.asList(new String[]{"Male", "Female", "Other"});
+    }
+
+    @ModelAttribute("login")
+    public Login getDefaultLogin() {
+        return new Login();
     }
 
 }
