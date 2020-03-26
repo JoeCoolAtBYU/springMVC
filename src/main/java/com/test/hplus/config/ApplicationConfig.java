f96827b7707ebd792b6e2fb113a22a0418ad314a
Joshua <joecoolatbyu@hotmail.com>
2020-03-26 16:18:51 -0600

    implementing interceptors

--- src/main/java/com/test/hplus/config/ApplicationConfig.java
+++ src/main/java/com/test/hplus/config/ApplicationConfig.java
@@ -1,6 +1,7 @@
 package com.test.hplus.config;
 
 import com.test.hplus.converters.StringToEnumConverter;
+import com.test.hplus.interceptors.LoggingInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +10,7 @@ import org.springframework.core.task.AsyncTaskExecutor;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -67,4 +69,10 @@ public class ApplicationConfig extends WebMvcConfigurationSupport {
 //        viewResolver.setBasename("views");
 //        return viewResolver;
 //    }
+
+
+    @Override
+    protected void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoggingInterceptor()).addPathPatterns("/*");
+    }
 }
